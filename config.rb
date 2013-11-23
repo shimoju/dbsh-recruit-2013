bb15faeb139f5fc8b8f41cfb5b1b134c948b0654
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2013-11-24 00:59:54 +0900

    ビルド時はリンクを相対パスにする

--- config.rb
+++ config.rb
@@ -73,6 +73,8 @@ configure :build do
   # Use relative URLs
   activate :relative_assets
 
+  set :relative_links, true
+
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 
